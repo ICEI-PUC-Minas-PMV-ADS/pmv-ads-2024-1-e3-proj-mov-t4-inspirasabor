32036c9fb3a97a1b7c996eb41e0ab7f937f110f5
TIAGO CARVALHO TAVEIRA ARAUJO <125419720+TiagoAraujo12@users.noreply.github.com>
2024-04-19 14:13:42 -0300

    Update Banco.sql
    Atualização dos scripts para atualização do tamanho dos campos NVARCHAR

--- src/bd/Banco.sql
+++ src/bd/Banco.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[Usuarios] (
     [idUsuario] INT           IDENTITY (1, 1) NOT NULL,
-    [nome]      NVARCHAR (50) NOT NULL,
-    [email]     NVARCHAR (25) NOT NULL,
+    [nome]      NVARCHAR (100) NOT NULL,
+    [email]     NVARCHAR (100) NOT NULL,
     [senha]     NVARCHAR (50) NOT NULL,
     CONSTRAINT [PK_Usuarios] PRIMARY KEY CLUSTERED ([idUsuario] ASC)
 );
@@ -13,8 +13,8 @@ CREATE TABLE [dbo].[Receitas] (
     [idUsuario]     INT            NOT NULL,
     [tituloReceita] NVARCHAR (50)  NOT NULL,
     [tipo]          CHAR (10)      NOT NULL,
-    [ingredientes]  NVARCHAR (500) NULL,
-    [modoPreparo]   NVARCHAR (500) NULL,
+    [ingredientes]  NVARCHAR (MAX) NULL,
+    [modoPreparo]   NVARCHAR (MAX) NULL,
     [nota]          FLOAT (53)     NULL,
     CONSTRAINT [PK_Receitas] PRIMARY KEY CLUSTERED ([idReceita] ASC),
     CONSTRAINT [FK_Receitas_Usuarios] FOREIGN KEY ([idUsuario]) REFERENCES [dbo].[Usuarios] ([idUsuario])
@@ -25,7 +25,7 @@ CREATE TABLE [dbo].[Receitas] (
 CREATE TABLE [dbo].[Comentarios] (
     [idReceitaComentario] INT            NOT NULL,
     [idUsuarioComentario] INT            NOT NULL,
-    [comentario]          NVARCHAR (200) NOT NULL,
+    [comentario]          NVARCHAR (MAX) NOT NULL,
     [idComentario]        INT            IDENTITY (1, 1) NOT NULL,
     CONSTRAINT [PK_Comentarios] PRIMARY KEY CLUSTERED ([idComentario] ASC),
     CONSTRAINT [FK_Comentarios_Receita] FOREIGN KEY ([idReceitaComentario]) REFERENCES [dbo].[Receitas] ([idReceita]),
