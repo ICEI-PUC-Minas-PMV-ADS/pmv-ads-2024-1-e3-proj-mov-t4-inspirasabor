32f11be8d6815348a90de34267854f3eae059242
ChrisCuri <christianecuri@gmail.com>
2024-05-05 17:24:43 -0300

    Atualização página adicionar receita.

--- src/pages/AdicionarReceita.js
+++ src/pages/AdicionarReceita.js
@@ -4,7 +4,8 @@ import Container from '../components/Container';
 import Body from '../components/Body';
 import Header from '../components/Header';
 import { useNavigation, useIsFocused } from '@react-navigation/native';
-import { RadioButton, Text } from 'react-native-paper';
+import { RadioButton, Text, TextInput, Button } from 'react-native-paper';
+import Input from "../components/Input";
 import { categoria } from '../services/categorias.service';
 //import jsonData from '../db/db.json'; 
 
@@ -12,6 +13,10 @@ const NovaReceita = () => {
 
     const navigation = useNavigation();
     const [AdicionarReceita, setAdicionarReceita] = useState([]);
+    const [tituloReceita, setTituloReceita] = useState([]);
+    const [ingredientes, setIngredientes] = useState([]);
+    const [modoPreparo, setModoPreparo] = useState([]);
+    
     const [categoria, setCategoria] = React.useState('Carnes');
     
     return (
@@ -23,33 +28,65 @@ const NovaReceita = () => {
             />
             <Body>
 
-            <Text style={styles.TituloCategoria}>Escolha uma Categoria:</Text>
+              <Text style={styles.Titulo}>1 - Escolha uma Categoria:</Text>
             
-            <View style={styles.ContainerRadio}>
+              <View style={styles.ContainerRadio}>
 
-            <RadioButton
-              value="Carnes"
-              status={ categoria === 'Carnes' ? 'checked' : 'unchecked' }
-              onPress={() => setCategoria('Carnes')}
-            />
-            <Text style={styles.NomeCategoria}>Carnes</Text>
+                <RadioButton
+                  value="Carnes"
+                  status={ categoria === 'Carnes' ? 'checked' : 'unchecked' }
+                  onPress={() => setCategoria('Carnes')}
+                />
+                <Text style={styles.NomeCategoria}>Carnes</Text>
 
-            <RadioButton
-              value="Doces"
-              status={ categoria === 'Doces' ? 'checked' : 'unchecked' }
-              onPress={() => setCategoria('Doces')}
-            />
-            <Text style={styles.NomeCategoria}>Doces</Text>
+                <RadioButton
+                  value="Doces"
+                  status={ categoria === 'Doces' ? 'checked' : 'unchecked' }
+                  onPress={() => setCategoria('Doces')}
+                />
+                <Text style={styles.NomeCategoria}>Doces</Text>
 
-            <RadioButton
-              value="Massas"
-              status={ categoria === 'Massas' ? 'checked' : 'unchecked' }
-              onPress={() => setCategoria('Massas')}
-            />
-            <Text style={styles.NomeCategoria}>Massas</Text>
+                <RadioButton
+                  value="Massas"
+                  status={ categoria === 'Massas' ? 'checked' : 'unchecked' }
+                  onPress={() => setCategoria('Massas')}
+                />
+                <Text style={styles.NomeCategoria}>Massas</Text>
 
             </View>
 
+            <Text style={styles.Titulo}>2 - Nome da Receita:</Text>
+            <Input
+                    value={tituloReceita}
+                    onChangeText={(text) => setTituloReceita(text)}
+                    left={<TextInput.Icon icon="lead-pencil" />}
+            />
+
+            <Text style={styles.Titulo}>3 - Ingredientes:</Text>
+            <Input
+                    value={ingredientes}
+                    onChangeText={(text) => setIngredientes(text)}
+                    left={<TextInput.Icon icon="food-apple" />}
+            />
+
+            <Text style={styles.Titulo}>4 - Modo de Preparo:</Text>
+            <Input
+                    value={modoPreparo}
+                    onChangeText={(text) => setModoPreparo(text)}
+                    left={<TextInput.Icon icon="food-fork-drink" />}
+            />
+
+            <Text style={styles.Titulo}>5 - Adicionar Imagem:</Text>
+
+
+            <Button 
+              icon='content-save-check' 
+              mode='conteined' 
+              color={'darkblue'}    
+              style={styles.Salvar}
+              onPress={() => console.log('A receita foi salva com sucesso!')}>SALVAR
+            </Button>
+            
             </Body>
 
       </Container>
@@ -62,14 +99,24 @@ const styles = StyleSheet.create({
     margin:8,
     alignItems:'center',    
  },
-  TituloCategoria:{
+  Titulo:{
     margin:8,
-    fontSize:20,
+    marginTop:18,
+    fontSize:18,
     fontWeight:'bold',
   },
   NomeCategoria:{
     fontSize:15,
     fontWeight:'bold',
+    color:'darkblue',
+  },
+  Salvar:{
+    marginTop: '15',
+    justifyContent: 'center',
+    marginHorizontal: 10,
+    borderBottomColor:'red',
+    fontSize:20,
+    backgroundColor: 'lightgreen',
   },
 });
 
