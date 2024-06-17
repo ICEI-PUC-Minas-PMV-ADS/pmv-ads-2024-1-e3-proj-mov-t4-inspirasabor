012f082cf197f14fa4ea29e3c13dfdb15c34384f
Ana Maria Pessoa <pessoaana.m@gmail.com>
2024-06-17 18:19:53 -0300

    fix: implementacao da funcionalidade de remover receitas dos favoritos

--- src/services/receita.service.js
+++ src/services/receita.service.js
@@ -52,6 +52,23 @@ export const updateReceita = async (param) => {
   }
 }
 
+export const deleteReceita = async (id) => {
+  try{
+    return await API.delete(`${BASE_URL}/receitas/${id}`).then( 
+      response => {
+        return response.data;
+      },
+      error =>{
+        console.log(error);
+        return  null;
+      }
+    );
+  }catch(error){
+    console.log(error);
+    return null;
+  }
+}
+
 export const removeFavorita = async (id) => {
   try{
     return await API.delete(`${BASE_URL}/receitas/${id}`).then( 
